006bc41d7195d4f887f1d673958333cff9365653
Federico Nahuel Encinaz Sayago <fencinazsayago@frba.utn.edu.ar>
2023-09-08 18:13:12 -0300

    Update init_kernel.h

--- MaPPA/kernel/include/init_kernel.h
+++ MaPPA/kernel/include/init_kernel.h
@@ -4,7 +4,7 @@
 #include "../../shared/include/sockets.h"
 #include "../../shared/include/protocolo.h"
 
-bool generar_conexiones(t_log* logger, int *md_memoria, int *md_cpu_dt, int *md_cpu_it, int *md_fs);
+bool generar_conexiones(t_log* logger, int* md_memoria, int* md_cpu_dt, int* md_cpu_it, int* md_fs);
 void cerrar_programa(t_log* logger);
 
 #endif
