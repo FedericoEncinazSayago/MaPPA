abf53616241c710717a5dc5f01b497064739da70
Federico Encinaz Sayago <fencinazsayago@frba.utn.edu.ar>
2023-09-09 05:58:28 +0000

    "Se puedo mandar informacion a Memoria y FS"

--- MaPPA/kernel/main.c
+++ MaPPA/kernel/main.c
@@ -3,15 +3,15 @@
 int main() {
     t_log* logger_kernel = log_create("kernel.log", "KERNEL", true, LOG_LEVEL_INFO);
 
-    int md_memoria = 0;
-
-    if(!generar_conexiones(logger_kernel, &md_memoria) {
+    int md_memoria = 0, md_fs;
+    
+    if(!generar_conexiones(logger_kernel, &md_memoria, &md_fs)) {
         log_error(logger_kernel, "No se pudieron generar las conexiones");
         return 1;
     }
-
+    
     send_notas(md_memoria, 10, 10);
-    // send_notas(md_fs, 20, 20);  Si anda bien md_memoria probamos con Filesystem
+    send_notas(md_fs, 20, 20);
 
     cerrar_programa(logger_kernel);
     
