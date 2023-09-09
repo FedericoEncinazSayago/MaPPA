2c5fcdee555d1956c762a47e2e037828112706b0
Diegote <diegoantonio40@live.com>
2023-09-09 21:09:45 +0000

    Se eliminan los Log y se realizaron las conexiones entre los modulos

--- MaPPA/kernel/main.c
+++ MaPPA/kernel/main.c
@@ -3,15 +3,22 @@
 int main() {
     t_log* logger_kernel = log_create("kernel.log", "KERNEL", true, LOG_LEVEL_INFO);
 
-    int md_memoria = 0, md_fs;
-    
-    if(!generar_conexiones(logger_kernel, &md_memoria, &md_fs)) {
+    int md_memoria = 0, md_cpu_dt = 0, md_cpu_it = 0, md_fs = 0;
+
+    if(!generar_conexiones(logger_kernel, &md_memoria, &md_cpu_dt, &md_cpu_it,&md_fs)) {
         log_error(logger_kernel, "No se pudieron generar las conexiones");
         return 1;
     }
+    uint8_t pruebadeConsola=0;
+    scanf("estamos probando :%hhu",&pruebadeConsola);
+    send_notas(md_memoria, 5, 10);
+    scanf("estamos probando :%hhu",&pruebadeConsola);
+    send_notas(md_fs,9, 10);
     
-    send_notas(md_memoria, 10, 10);
-    send_notas(md_fs, 20, 20);
+    scanf("estamos probando :%hhu",&pruebadeConsola);
+    send_notas(md_cpu_dt, 7, 10);
+    scanf("estamos probando :%hhu",&pruebadeConsola);
+    send_notas(md_cpu_it,8, 10);
 
     cerrar_programa(logger_kernel);
     
