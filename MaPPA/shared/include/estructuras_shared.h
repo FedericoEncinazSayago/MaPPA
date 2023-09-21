544f336e09b5f4a0df5d7ceaa5f84159b5edf4a9
Federico Encinaz Sayago <fencinazsayago@frba.utn.edu.ar>
2023-09-21 17:01:22 -0300

    Creacion de PCB y Registros de uso General de CPU

--- MaPPA/shared/include/estructuras_shared.h
+++ MaPPA/shared/include/estructuras_shared.h
@@ -1,12 +1,27 @@
 #ifndef ESTRUCTURAS_SHARED_H_
 #define ESTRUCTURAS_SHARED_H_
 
-#include <commons/log.h> 
+#include <commons/log.h>
+#include <stdint.h>
 
 typedef struct {
     t_log* logger;
     char* server_name;
     int socket_cliente;
 } t_procesar_conexion;
+
+typedef struct { // Registro de la CPU
+    uint32_t AX;
+    uint32_t BX;
+    uint32_t CX;
+    uint32_t DX;
+} t_registros_cpu;
+
+typedef struct { // PCB de un proceso
+    int pid;
+    int program_counter;
+    t_registros_cpu registros;
+    char** archivos_abiertos; 
+}t_pcb;
 
 #endif /* ESTRUCTURAS_SHARED_H_ */
