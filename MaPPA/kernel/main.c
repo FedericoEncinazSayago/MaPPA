68f5a75a5ee264fb9aeebce8a7dc2653c6c1df20
Federico Encinaz Sayago <fencinazsayago@frba.utn.edu.ar>
2023-09-21 17:04:17 -0300

    Modificacion de main de Memoria y Kernel (Todavia esta en proceso!)

--- MaPPA/kernel/main.c
+++ MaPPA/kernel/main.c
@@ -16,22 +16,22 @@ int main() {
     t_log* logger_kernel = log_create("kernel.log", "KERNEL", true, LOG_LEVEL_INFO);
     inicializar_config(); // Inicializo la variable global config_kernel! -> No se si es la mejor forma de hacerlo!
 
+    // NEW = crear_cola() // Hay que armar la PCB! 
+    // READY = crear_cola()
+    // EXEC = crear_cola()
+    // BLOCK = crear_cola()
+    // EXIT = crear_cola()
+
     int md_memoria = 0, md_cpu_dt = 0, md_cpu_it = 0, md_fs = 0;
 
-    if(!cargar_configuraciones(config_kernel, logger_kernel) || !generar_conexiones(logger_kernel, config_kernel, &md_memoria, &md_cpu_dt, &md_cpu_it, &md_fs)) {
+    if(!cargar_configuraciones(config_kernel, logger_kernel) || !generar_conexiones(logger_kernel, config_kernel, &md_memoria, &md_cpu_dt, &md_cpu_it, &md_fs)) { // Generar conexiones, no va a mantener la conexion, sino que va a crear la conexion y la va a cerrar!
         log_error(logger_kernel, "No se pudieron generar las conexiones");
 
         return 1;
     }
 
-    /*
-    send_notas(md_memoria, 10, 10);
-    send_notas(md_cpu_dt, 10, 10);
-    send_notas(md_cpu_it, 10, 10);
-    send_notas(md_fs, 10, 10);
-    */
-
-    //recorrer_vector(config_kernel->recursos);
+    // Fede en momento de insipiracion!
+    iniciar_modulo() // Funcion en proceso de creacion! 
 
     cerrar_programa(logger_kernel);
 
--- MaPPA/memoria/main.c
+++ MaPPA/memoria/main.c
@@ -1,11 +1,26 @@
 #include "include/main.h"
 
+t_config_memoria* config_memoria;
+
+void inicializar_config(void) { // Voy a inicializar config de memoria!s
+    config_memoria = malloc(sizeof(t_config_memoria));
+    config_memoria->ip_fs = NULL;
+    config_memoria->ip_escucha = NULL;
+    config_memoria->path_instrucciones = NULL;
+    config_memoria->algoritmo_reemplazo = NULL;
+} 
 
 int main() {
 
     t_log* logger_m = log_create("memoria.log", "MEMORIA", true, LOG_LEVEL_INFO);
 
-    
+    if(!cargar_configuraciones(config_memoria, logger_m)) { // Traigo las configuraciones de memoria!
+        log_error(logger_m, "No se pudieron cargar las configuraciones");
+
+        return 1;
+    }
+
+    iniciar_modulo() // Acá voy a crear el servidor con las cosas!
 
     cerrar_programa(logger_m);
 
