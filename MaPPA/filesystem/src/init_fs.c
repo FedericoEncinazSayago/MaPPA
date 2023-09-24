230b8ade003c3f5a6aac25604bd0b8febbdd2ac3
Federico Encinaz Sayago <fencinazsayago@frba.utn.edu.ar>
2023-09-23 21:26:12 -0300

    Cambios esteticos en Filesystem :)

--- MaPPA/filesystem/src/init_fs.c
+++ MaPPA/filesystem/src/init_fs.c
@@ -1,7 +1,7 @@
 #include "../include/init_fs.h"
 
 bool cargar_configuraciones( t_config_fs* config_fs, t_log* logger) {
-    t_config* config_fs_open = config_create("fs.config");
+    t_config* config_f = config_create("fs.config");
 
     if(config_fs == NULL) {
         log_error(logger, "No se pudo cargar la configuracion del filesystem");
@@ -24,27 +24,28 @@ bool cargar_configuraciones( t_config_fs* config_fs, t_log* logger) {
         NULL
     };
 
-    if(!tiene_todas_las_configuraciones(config_fs, configuraciones)) {
+    if(!tiene_todas_las_configuraciones(config_f, configuraciones)) {
         log_error(logger, "No se pudo cargar la configuracion del filesystem");
 
         return false;
     }
     
 
-    copiar_valor(&config_fs->ip_memoria, config_get_string_value(config_fs_open, "IP_MEMORIA"));
-    config_fs->puerto_memoria = config_get_int_value(config_fs_open, "PUERTO_MEMORIA");
-    config_fs->puerto_escucha = config_get_int_value(config_fs_open, "PUERTO_ESCUCHA");
-    copiar_valor(&config_fs->path_fath, config_get_string_value(config_fs_open, "PATH_FAT"));
-    copiar_valor(&config_fs->path_bloques, config_get_string_value(config_fs_open, "PATH_BLOQUES"));
-    copiar_valor(&config_fs->path_fcb, config_get_string_value(config_fs_open, "PATH_FCB"));
-    config_fs->cant_bloques_total = config_get_int_value(config_fs_open, "CANT_BLOQUES_TOTAL");
-    config_fs->cant_bloques_swap = config_get_int_value(config_fs_open, "CANT_BLOQUES_SWAP");
-    config_fs->tam_bloque = config_get_int_value(config_fs_open, "TAM_BLOQUE");
-    config_fs->retardo_acceso_bloque = config_get_int_value(config_fs_open, "RETARDO_ACCESO_BLOQUE");
-    config_fs->retardo_acceso_fat = config_get_int_value(config_fs_open, "RETARDO_ACCESO_FAT");
+    copiar_valor(&config_fs->path_fcb, config_get_string_value(config_f, "PATH_FCB"));
+    copiar_valor(&config_fs->path_bloques, config_get_string_value(config_f, "PATH_BLOQUES"));
+    copiar_valor(&config_fs->path_fath, config_get_string_value(config_f, "PATH_FAT"));
+    copiar_valor(&config_fs->ip_memoria, config_get_string_value(config_f, "IP_MEMORIA"));
+
+    config_fs->puerto_memoria = config_get_int_value(config_f, "PUERTO_MEMORIA");
+    config_fs->puerto_escucha = config_get_int_value(config_f, "PUERTO_ESCUCHA");
+    config_fs->cant_bloques_total = config_get_int_value(config_f, "CANT_BLOQUES_TOTAL");
+    config_fs->cant_bloques_swap = config_get_int_value(config_f, "CANT_BLOQUES_SWAP");
+    config_fs->tam_bloque = config_get_int_value(config_f, "TAM_BLOQUE");
+    config_fs->retardo_acceso_bloque = config_get_int_value(config_f, "RETARDO_ACCESO_BLOQUE");
+    config_fs->retardo_acceso_fat = config_get_int_value(config_f, "RETARDO_ACCESO_FAT");
 
     log_info(logger, "Configuraciones cargadas correctamente");
-    config_destroy(config_fs_open);
+    config_destroy(config_f);
 }
 
 void cerrar_programa(t_log* logger) {
