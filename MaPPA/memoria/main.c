5209f50f9b674ba3138f3a1c8c6cf9c842ee0c27
Federico Nahuel Encinaz Sayago <fencinazsayago@frba.utn.edu.ar>
2023-09-26 00:08:45 -0300

    Cambio estetico de Memoria :)

--- MaPPA/memoria/main.c
+++ MaPPA/memoria/main.c
@@ -28,17 +28,7 @@ int main() {
 
         return 1;
     }
-
-    t_pcb* pcb = malloc(sizeof(t_pcb));
-    pcb->pid = 1;
-    pcb->program_counter = 0;
-    pcb->registros = malloc(sizeof(t_registros_cpu));
-    pcb->registros->AX = 0;
-    pcb->registros->BX = 0;
-    pcb->registros->CX = 0;
-    pcb->registros->DX = 0;
-    pcb->archivos_abiertos = list_create();
-
+    
     iniciar_modulo(logger_m, config_memoria); // Acá voy a crear el servidor con las cosas!
 
     cerrar_programa(logger_m);
