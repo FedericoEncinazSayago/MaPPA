65ee0b1709317c9166f6f303b4007d16bdb3982b
Federico Encinaz Sayago <fencinazsayago@frba.utn.edu.ar>
2023-09-27 16:59:12 -0300

    Cambios a las consolas, y comienzo con las pruebas :)

--- MaPPA/kernel/include/consola.h
+++ MaPPA/kernel/include/consola.h
@@ -1,11 +1,25 @@
 #ifndef CONSOLA_H_
 #define CONSOLA_H_
 
-#include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <sys/types.h>
+#include <sys/file.h>
+#include <sys/stat.h>
+#include <sys/errno.h>
 #include <readline/readline.h>
+#include <readline/history.h>
 
-void iniciarConsola();
+typedef struct {
+    char* nombre;
+    void* funcion;
+} COMMAND;
+
+void iniciar_consola();
+char* eliminar_espacio(char* cadena);
+int ejecutar_comando(char* linea);
+COMMAND* encontrar_comando(char* nombre);
+void iniciar_readline();
+char* generador_de_comandos(const char* texto, int estado);
+char** completar_MaPPA(const char* texto, int inicio, int fin);
 
 #endif
--- MaPPA/kernel/src/consola.c
+++ MaPPA/kernel/src/consola.c
@@ -1,45 +1,141 @@
 #include "../include/consola.h"
-// OBJETIVOS 
-void iniciarConsola(){
-    char * operacion =NULL;
-    char* linea = NULL;
-    operacion=(char *)malloc(100);
-    while(1){ 
-        linea = readline("> ");
-        printf("linea: %s\n", linea);
-        if(sscanf(linea, "%99s", operacion)== 1) {
-            if (strcmp(operacion, "h") == 0) {
-                printf("A DOMIr\n");
-            }
 
+COMMAND comandos[] = {
+    {"INICIAR_PROCESO", iniciar_proceso},
+    /*{"FINALIZAR_PROCESO", finalizar_proceso},
+    {"DETENER_PLANIFICACION", detener_planificacion},
+    {"INICIAR_PLANIFICACION", iniciar_planificacion},
+    {"MULTIPROGRAMACION", multiprogramacion},
+    {"PROCESO_ESTADO", proceso_estado},
+    {"HELP", help},*/
+    {NULL, NULL}
+};
 
-            if (operacion=="INICIAR_PROCESO"){
-                printf("Entre a Inciar proceso");
-                //create hilo 
-                    //t_pcb* pcb = inciarPCB()
-                    //ingresarNew(pcb);
-                //join hilo
-            }else if (operacion = "FINALIZAR_PROCESO"){
-                printf("Entre Finalizar proceso");
-                //ingresarExit(PID);
-            }else if(operacion = "DETENER_PLANIFICACION"){
-                printf("Entre DETENER_PLANIFICACION");
+void iniciar_consola() {
+    char* linea;
+    char* operacion;
 
-            }else if(operacion = "INICIAR_PLANIFICACION"){
-                printf("Entre INICIAR_PLANIFICACION");
+    iniciar_readline();
 
-            }else if(operacion = "MULTIPROGRAMACION"){
-                printf("Entre MULTIPROGRAMACION");
+    while(1) {
+        linea = readline("MaPPA: ");
 
-            }else if(operacion = "PROCESO_ESTADO"){
-                printf("Entre PROCESO_ESTADO");
+        if(!linea)
+            break;
 
-            }else {
-                printf("La funcion no existe!\n");
+        operacion = eliminar_espacios(linea);
 
-            }
-        } else {
-            printf("No se pudo leer la palabra.\n");
-        }       
+        if(operacion != NULL) {
+            add_history(linea);
+            ejecutar_comando(linea);
+        }
+           
+        free(linea);
     }
+}
+
+char* eliminar_espacio(char* cadena) {
+    char* aux = cadena;
+    char* aux2;
+
+    // Elimina los espacios en blanco al principio de la cadena
+    while (*aux && isspace(*aux))
+        aux++;
+
+    if (*aux == '\0') {
+        // Si la cadena queda vacía después de eliminar espacios en blanco,
+        // se retorna una cadena vacía
+        return aux;
+    }
+
+    aux2 = aux + strlen(aux) - 1;
+
+    // Elimina los espacios en blanco al final de la cadena
+    while (aux2 > aux && isspace(*aux2))
+        aux2--;
+
+    // Coloca el carácter nulo '\0' después del último carácter no espacio en blanco
+    *(++aux2) = '\0';
+
+    // Retorna la dirección del primer carácter no espacio en blanco
+    return aux;
+}
+
+
+int ejecutar_comando(char* linea) {
+    int i = 0;
+    char* palabra;
+    COMMAND* comando;
+
+    while(linea[i] && isspace(linea[i]))
+        i++;
+    
+    palabra = linea + i;
+
+    while(linea[i] && !isspace(linea[i]))
+        i++;
+    
+    if(linea[i])
+        linea[i++] = '\0';
+
+    comando = encontrar_comando(palabra);
+
+    if(!comando) {
+        printf("Comando desconocido: %s\n", palabra);
+
+        return -1;
+    }
+
+    while(linea[i] && isspace(linea[i]))
+        i++;
+
+    palabra = linea + i;
+
+    return (*(comando->funcion) (palabra));
+}
+
+COMMAND* encontrar_comando(char* nombre) {
+    for(int i = 0; comandos[i].nombre; i++) {
+        if(strcmp(nombre, comandos[i].nombre) == 0)
+            return &comandos[i];
+    }
+
+    return ((COMMAND*) NULL);
+}
+
+void iniciar_readline() {
+    rl_readline_name = "MaPPA";
+    rl_attempted_completion_function = completar_MaPPA;
+}
+
+char** completar_MaPPA(const char* texto, int inicio, int fin) {
+    char** matches = NULL;
+
+    if(inicio == 0)
+        matches = rl_completion_matches(texto, generador_de_comandos);
+
+    return (matches);
+}
+
+char* generador_de_comandos(const char* texto, int estado) {
+    static int lista_index, len;
+    char* nombre;
+
+    if(!estado) {
+        lista_index = 0;
+        len = strlen(texto);
+    }
+
+    while((nombre = comandos[lista_index].nombre)) {
+        lista_index++;
+
+        if(strncmp(nombre, texto, len) == 0)
+            return strdup(nombre);
+    }
+
+    return ((char*) NULL);
+}
+
+void* iniciar_proceso(void* args) {
+    printf("Iniciando proceso...\n");
 }
